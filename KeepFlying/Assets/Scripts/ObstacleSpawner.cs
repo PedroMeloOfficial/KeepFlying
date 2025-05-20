5f25e96be82b27c77ee0c8e811c12b9f228c9251
thariqmirza94 <thariqmirza94@gmail.com>
2025-05-20 15:09:29 -0700

    Implementing game assets

--- KeepFlying/Assets/Scripts/ObstacleSpawner.cs
+++ KeepFlying/Assets/Scripts/ObstacleSpawner.cs
@@ -2,34 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeSpawner : MonoBehaviour
+public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField] private float _maxTime = 1.5f;
     [SerializeField] private float _heightRange = 0.45f;
-    [SerializeField] private GameObject _pipe;
+    [SerializeField] private GameObject _obstacle;
 
     private float _timer;
 
     private void Start()
     {
-        SpawnPipe();
+        SpawnObstacle();
     }
 
     private void Update()
     {
         if(_timer > _maxTime)
         {
-            SpawnPipe();
+            SpawnObstacle();
             _timer = 0;
         }
         _timer += Time.deltaTime;
     }
 
-    private void SpawnPipe()
+    private void SpawnObstacle()
     {
         Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-_heightRange, _heightRange));
-        GameObject pipe = Instantiate(_pipe, spawnPos, Quaternion.identity);
+        GameObject obstacle = Instantiate(_obstacle, spawnPos, Quaternion.identity);
 
-        Destroy(pipe, 10f); 
+        Destroy(obstacle, 10f); 
     }
 }
