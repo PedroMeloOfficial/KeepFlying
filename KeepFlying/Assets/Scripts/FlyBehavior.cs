f5f40dc84fb847d85a4a9d4a44b77b8e7ba7bc76
BeomjunCho <chobj555@gmail.com>
2025-05-20 23:18:42 -0700

    Added animation for fly and dead Fixed ui scale bug Changed music

--- KeepFlying/Assets/Scripts/FlyBehavior.cs
+++ KeepFlying/Assets/Scripts/FlyBehavior.cs
@@ -10,18 +10,28 @@ public class FlyBehavior : MonoBehaviour
     [SerializeField] private float _rotationSpeed = 10f;
     [SerializeField] private float _maxAltitude = 1.1f;
 
+    [Header("References")]
+    [SerializeField] private Animator _animator;     // Drag your Animator here in the Inspector
+
     private Rigidbody2D _rb;
     private Transform _transform;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _transform = GetComponent<Transform>();
+        _transform = transform;
+
+        // If you didn¡¯t hook it up in the Inspector, try to grab it at runtime:
+        if (_animator == null)
+        {
+            _animator = GetComponent<Animator>();
+            print("Got animator");
+        }
+            
     }
 
     private void Update()
     {
-        // On click, apply upward velocity (capped by maxAltitude)
         if (Mouse.current.leftButton.wasPressedThisFrame &&
             _transform.position.y <= _maxAltitude)
         {
@@ -31,13 +41,22 @@ public class FlyBehavior : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Rotate plane based on vertical speed
         float angle = _rb.velocity.y * _rotationSpeed;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private IEnumerator HandleCrash()
     {
+        _animator.SetTrigger("Crumpled");
+        _rb.simulated = false;
+        // Wait for the length of the crumple clip:
+        yield return new WaitForSeconds(0.05f);
         GameManager.Instance.GameOver();
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(HandleCrash());
+    }
+
 }
